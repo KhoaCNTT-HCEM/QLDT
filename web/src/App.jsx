85867d926edde813963a3d6dc973ea7c0e9a10a6
LeeZuy <147964153+LeeZuy@users.noreply.github.com>
2025-04-23 14:26:47 +0700

    update dk, dn

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './components/loginPage';
 import RegisterPage from './components/registerPage';
+import LogoHeader from './icon/LogoHeader/LogoHeader.jsx';
 
 import './App.css'
 
@@ -9,10 +10,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
+    <div className='container'>
+      {/* <header>
+        <LogoHeader/>
+      </header> */}
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        {/* <Route path='/logoHeader' element={<LogoHeader />} /> */}
       </Routes>
     </div>
   )
