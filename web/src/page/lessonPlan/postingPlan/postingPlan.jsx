2e4a789ead6ab69bd2335b35c1fa0559de4f898f
LeeZuy <147964153+LeeZuy@users.noreply.github.com>
2025-04-26 14:58:01 +0700

    up file

---
+++ web/src/page/lessonPlan/postingPlan/postingPlan.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './style.css';
+import { InboxOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+
+const { Dragger } = Upload;
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+
+const PostingPlan = () => {
+  return (
+    <div className='postingPlan'>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Nhấn chuột hoặc thả file vào đây để đăng tải giáo án</p>
+        {/* <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+          banned files.
+        </p> */}
+      </Dragger>
+    </div>
+  )
+}
+
+export default PostingPlan
